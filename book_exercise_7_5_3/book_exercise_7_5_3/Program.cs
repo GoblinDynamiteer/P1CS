1cc138e8a3169e7dfdcb61f76c56c69e2c477ac9
Johan Kämpe <kampe.johan@gmail.com>
2017-06-22 20:32:03 +0200

    Book exercise 7.5.3 done

---
+++ book_exercise_7_5_3/book_exercise_7_5_3/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Programmering 1 C# Arbetsbok
+ * Exercise 7.5.3
+ * Page 19
+ * 
+ * Exercise notes:
+ * Create a program: Heads or tails
+ * USER INPUT: Number of coin flips
+ * OUTPUT: Result of each coin flip
+ * 
+ * Note: Investigate how to randomize in C#
+ *  
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_7_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Random class */
+            Random coinFlip = new Random();
+
+            Console.Write("Enter amount of coin flips: ");
+
+            int flips = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while (flips-- != 0)
+            {
+                /* Next(0,2) returns random number between 
+                 * 0 and 2 (0 or 1) */
+                string result = (coinFlip.Next(0, 2) == 1 ) ?
+                    "Heads!" : "Tails";
+
+                Console.WriteLine("Flip #{0}: {1}", 
+                    counter++.ToString("000"),
+                    result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

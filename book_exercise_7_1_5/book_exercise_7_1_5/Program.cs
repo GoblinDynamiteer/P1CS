b9df07bcfa3de43f5f06847bce3f4edf951dbbfb
Johan Kämpe <kampe.johan@gmail.com>
2017-06-22 11:04:59 +0200

    Book exercise 7.1.5 done

---
+++ book_exercise_7_1_5/book_exercise_7_1_5/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Programmering 1 C# Arbetsbok
+ * Exercise 7.1.5
+ * Page 14
+ * 
+ * Exercise notes:
+ * Create a program:
+ * input: two person names
+ * output: a short story about the persons
+ * 
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_7_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameA, nameB;
+
+            Console.Write("Enter a name: ");
+            nameA = Console.ReadLine();
+
+            Console.Write("Enter another name: ");
+            nameB = Console.ReadLine();
+
+            /* The story */
+            Console.WriteLine("{0} and {1} are friends!" +
+                "\n{1} likes lettuce!", nameA, nameB);
+
+            Console.Read();
+        }
+    }
+}

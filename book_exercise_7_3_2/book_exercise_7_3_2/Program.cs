eaeed960ea0ed42a76faf9f0a26b4f960239ed5b
Johan Kämpe <kampe.johan@gmail.com>
2017-06-22 17:38:55 +0200

    Book exercise 7.3.2 done

---
+++ book_exercise_7_3_2/book_exercise_7_3_2/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Programmering 1 C# Arbetsbok
+ * Exercise 7.3.2
+ * Page 16
+ * 
+ * Exercise notes:
+ * Create a program: Check the weather (part 2):
+ * 
+ * Add feature to program: Check the weather (part 1):
+ * IF USER INPUT == "n"
+ *      OUTPUT "Let's stay inside and read a book!"
+ *      
+ * Note: Input shall be case insensitive
+ *  
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_7_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char answer;
+            Console.WriteLine("Is it nice weather today? (y)");
+
+            answer = Console.ReadKey(true).KeyChar;
+
+            /* Char.ToLower() converts char to lowercase */
+            if (Char.ToLower(answer) == 'y')
+            {
+                Console.WriteLine("Let's go on a picnic!");
+                Console.ReadLine();
+            }
+
+            else if (Char.ToLower(answer) == 'n')
+            {
+                Console.WriteLine("Let's stay inside and read a book!");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}

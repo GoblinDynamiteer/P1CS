2e86bec10fe36cb06e0a573c8ac436d9a5953ca9
Johan Kämpe <kampe.johan@gmail.com>
2017-06-22 11:44:52 +0200

    Book exercise 7.1.6 done

---
+++ book_exercise_7_1_6/book_exercise_7_1_6/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Programmering 1 C# Arbetsbok
+ * Exercise 7.1.6
+ * Page 14
+ * 
+ * Exercise notes:
+ * Create a program:
+ * input: one floating point value, eg. 11,534 
+ * output: rounded value, two digit precision
+ * 
+ * note: use Math.Round()
+ * 
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_7_1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value;
+
+            Console.Write("Enter a floating point value: ");
+            value = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The value {0} is rounded " +
+                "to value {1}",
+                value, Math.Round(value, 2) );
+
+            Console.Read();
+
+        }
+    }
+}

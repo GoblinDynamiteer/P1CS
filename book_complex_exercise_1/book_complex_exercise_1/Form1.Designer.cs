0bf7ac68193c20619e5db6e8588d5db87c5c7b15
Johan Kämpe <kampe.johan@gmail.com>
2017-06-23 00:21:58 +0200

    Book Complex exercise 1 done

---
+++ book_complex_exercise_1/book_complex_exercise_1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace book_complex_exercise_1
+{
+    partial class GuessNumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxGuess = new System.Windows.Forms.TextBox();
+            this.ButtonGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(28, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 46);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter number (1-100)";
+            // 
+            // TextBoxGuess
+            // 
+            this.TextBoxGuess.Location = new System.Drawing.Point(137, 57);
+            this.TextBoxGuess.Name = "TextBoxGuess";
+            this.TextBoxGuess.Size = new System.Drawing.Size(80, 20);
+            this.TextBoxGuess.TabIndex = 0;
+            this.TextBoxGuess.TextChanged += new System.EventHandler(this.TextBoxGuess_TextChanged);
+            // 
+            // ButtonGuess
+            // 
+            this.ButtonGuess.Location = new System.Drawing.Point(137, 84);
+            this.ButtonGuess.Name = "ButtonGuess";
+            this.ButtonGuess.Size = new System.Drawing.Size(80, 23);
+            this.ButtonGuess.TabIndex = 3;
+            this.ButtonGuess.Text = "Guess!";
+            this.ButtonGuess.UseVisualStyleBackColor = true;
+            this.ButtonGuess.Click += new System.EventHandler(this.ButtonGuess_Click);
+            // 
+            // GuessNumber
+            // 
+            this.AcceptButton = this.ButtonGuess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 137);
+            this.Controls.Add(this.ButtonGuess);
+            this.Controls.Add(this.TextBoxGuess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "GuessNumber";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess the number!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxGuess;
+        private System.Windows.Forms.Button ButtonGuess;
+    }
+}
+
---
+++ book_complex_exercise_1/book_complex_exercise_1/Form1.cs
@@ -0,0 +1,115 @@
+﻿/* Programmering 1 C# Arbetsbok   
+ * Complex Exercise 1
+ * Page 33 
+ *    
+ * Exercise notes: 
+ * Create a guessing game with a GUI
+ * 
+ * Generate a random number (1-100)
+ * Have user enter guesses in a textbox
+ * 
+ * Display text telling if number is 
+ * higher or lower than guess
+ *     
+ *     
+ * Johan Kämpe   
+ * https://github.com/GoblinDynamiteer/   
+ * https://www.linkedin.com/in/johankampe/   
+ *    
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace book_complex_exercise_1
+{
+    public partial class GuessNumber : Form
+    {
+        GuessingGame Game = new GuessingGame();
+
+        public GuessNumber()
+        {
+            InitializeComponent();
+
+            Game.GenerateNewNumber();
+            TextBoxGuess.Text = "0";
+            TextBoxGuess.Focus();
+            TextBoxGuess.SelectAll();
+            textBox1.Select(0, 0);
+        }
+
+        private void TextBoxGuess_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonGuess_Click(object sender, EventArgs e)
+        {
+            int userGuess;
+
+            if (int.TryParse(TextBoxGuess.Text, out userGuess))
+            {
+                /* Check can 0: num is lower, 1: num is higher, 
+                 * 2: guess is correct! */
+                int guessCheck = Game.CheckGuess(userGuess);
+                if (guessCheck == 2)
+                {
+                    /* Pop a message box and reset game */
+                    MessageBox.Show("Correct! " +
+                        "New random number generated");
+                    Game.GenerateNewNumber();
+                    TextBoxGuess.Clear(); // Clear guess textbox
+                }
+
+                else
+                {
+                    string text = (guessCheck == 0) ? 
+                        "lower!" : "higher";
+
+                    textBox1.Text = string.Format(
+                        "You guessed {0}\r\nThe number is {1}", 
+                            userGuess, 
+                            text);
+                }
+
+                /* Select guess textbox and 
+                 * focus text cursor */
+                TextBoxGuess.Focus();
+                TextBoxGuess.SelectAll();
+            }
+            
+        }
+
+    }
+
+    public class GuessingGame
+    {
+        static Random random = new Random();
+        int number;
+
+        public void GenerateNewNumber()
+        {
+            number = random.Next(1, 101);
+        }
+
+        /* Return 2 if guess is correct
+         * Return 1 if guess is too high
+         * return 0 if guess is too low */
+        public int CheckGuess(int guess)
+        {
+            int retVal = 2;
+            if (guess != number)
+            {
+                retVal = (guess > number) ? 0 : 1;
+            }
+            return retVal;
+        }
+    }
+}

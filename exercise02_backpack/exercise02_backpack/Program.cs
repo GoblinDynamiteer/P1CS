31ec23f3dc37092598e92591924f92afaaf75b48
Johan Kämpe <kampe.johan@gmail.com>
2017-06-24 12:28:06 +0200

    Course Exercise 02 "The backpack", init with instructions

---
+++ exercise02_backpack/exercise02_backpack/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Övning 2 - Ryggsäcken
+ * Programmering 1
+ * 
+ * Exercise instructions:
+ *  Create a "backpack" in in wich the user
+ *  can add an item to it by entering a 
+ *  string value.
+ *  
+ *  The program shall display a menu:
+ *  (Translated to English)
+ *  
+ *  Welcome to the backpack!
+ *  [1] Add an item
+ *  [2] Display backpack contents
+ *  [3] Clear backpack contents
+ *  [4] Quit
+ *  Choose:
+ * 
+ *  -   Items added with menu item 1 
+ *      shall be saved
+ *  -   Added items shall be displayed 
+ *      with menu item 2
+ *  -   Added items shall be cleares 
+ *      with menu item 3
+ *  
+ * Extra:
+ *  -   Handle incorrect user input
+ *  -   Save items to an array or list
+ *  -   Use classes and methods to build 
+ *      the program
+ *
+ * Johan Kämpe
+ * 2017-06-24
+ * https://github.com/GoblinDynamiteer/  
+ * https://www.linkedin.com/in/johankampe/  
+ *   
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise02_backpack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

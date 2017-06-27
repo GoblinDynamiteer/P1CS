2c06742a5a4a72d01740b0a424e47bf1fb3821af
Johan Kämpe <kampe.johan@gmail.com>
2017-06-27 15:57:58 +0200

    Book exercise 13.3 - 13.5 done

---
+++ book_exercise_13_3_to_13_5/book_exercise_13_3_to_13_5/PetDragon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_13_3_to_13_5
+{
+    class PetDragon
+    {
+        /* Gold in your dragon's hoard */
+        private UInt64 treasure = 0;
+        static Random rand = new Random();
+        
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        /* Constructor with default values */
+        public PetDragon(string name = "Draco", 
+            int age = 130, string color = "Green")
+        {
+            this.age = age;
+            this.color = color;
+            this.name = name;
+        }
+
+
+        /* Method for collecting treasure 
+         * to the dragon's hoard */
+        public void CollectTreasure()
+        {
+            int found = rand.Next(0, (int)UInt16.MaxValue);
+            this.treasure += (ulong)found;
+            Console.WriteLine("{0} flew on treasure hunt and " +
+                "gathered {1} gold pieces.\nFor a total of {2}.", 
+                    this.name, found, this.treasure);
+        }
+
+        /* Display info about your dragon */
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: {0}.", 
+                this.name);
+            Console.WriteLine("Spieces: {0} Dragon.", 
+                this.color);
+            Console.WriteLine("Age: {0} years.", 
+                this.age);
+            Console.WriteLine("Treasure: {0} gold pieces.", 
+                this.treasure);
+
+        }
+    }
+}
---
+++ book_exercise_13_3_to_13_5/book_exercise_13_3_to_13_5/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Programmering 1 C# Arbetsbok 
+ * Exercise 13.3 to 13.5 
+ * Page 28
+ *  
+ * Exercise notes: 
+ * Create a class of your favorite animal
+ * 
+ * (Part 1)
+ * Create class members, variables and methods.
+ * E.g. for a dog: 
+ *      Variables: age, name, breed
+ *      Methods: Bark(), Fetch()
+ *   
+ * (Part 2)
+ * Make the variables private, set them with
+ * get/set.
+ * 
+ * (Part 3)
+ * Make a constructor for your animal class.
+ * 
+ * Johan Kämpe 
+ * https://github.com/GoblinDynamiteer/ 
+ * https://www.linkedin.com/in/johankampe/ 
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_13_3_to_13_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PetDragon myDragon = new PetDragon("Fanny", 1304, "Red");
+
+            myDragon.DisplayInfo();
+
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                myDragon.CollectTreasure();
+            }
+
+            Console.WriteLine();
+            myDragon.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}

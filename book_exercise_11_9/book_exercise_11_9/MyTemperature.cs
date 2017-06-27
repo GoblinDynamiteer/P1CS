f6bfe90175758b95e107440a7d225d37cfc559d9
Johan Kämpe <kampe.johan@gmail.com>
2017-06-27 10:10:09 +0200

    Book exercise 11.9 done

---
+++ book_exercise_11_9/book_exercise_11_9/MyTemperature.cs
@@ -0,0 +1,76 @@
+﻿/* Programmering 1 C# Arbetsbok 
+ * Exercise 7.5.5 
+ * Page 20
+ *  
+ * Exercise notes: 
+ * Redesign the code in example 11.15.
+ * Build an internal method for the class
+ * MyTemperature.cs. The method shall be
+ * private. Name the method CTF();
+ * 
+ * Also, built another method FTC(),
+ * that converts degrees in fahrenheit
+ * to celsius.
+ * 
+ *   
+ * Johan Kämpe 
+ * https://github.com/GoblinDynamiteer/ 
+ * https://www.linkedin.com/in/johankampe/ 
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_11_9
+{
+    class MyTemperature
+    {
+        public static bool IsBoiling(int temp)
+        {
+            return (temp > 100);
+        }
+
+        public static bool IsBoiling(float temp)
+        {
+            return (temp > 100);
+        }
+
+        public static int CelsiusToFahrenheit(int c)
+        {
+            float f = CTF((float)c);
+            return (int)Math.Round(f);
+        }
+
+        public static float CelsiusToFahrenheit(float c)
+        {
+            return CTF(c);
+        }
+
+        public static float FahrenheitToCelcius(float f)
+        {
+            return FTC(f);
+        }
+
+        public static int FahrenheitToCelcius(int f)
+        {
+            float c = FTC((float)f);
+            return (int)Math.Round(c);
+        }
+
+        /* Only visible inside class MyTemperature */
+        private static float CTF(float c)
+        {
+            return c * 9 / 5 + 32;
+        }
+
+        private static float FTC(float f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+
+    }
+}
---
+++ book_exercise_11_9/book_exercise_11_9/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Programmering 1 C# Arbetsbok 
+ * Exercise 7.5.5 
+ * Page 20
+ *  
+ * Exercise notes: 
+ * Redesign the code in example 11.15.
+ * Build an internal method for the class
+ * MyTemperature.cs. The method shall be
+ * private. Name the method CTF();
+ * 
+ * Also, built another method FTC(),
+ * that converts degrees in fahrenheit
+ * to celsius.
+ * 
+ *   
+ * Johan Kämpe 
+ * https://github.com/GoblinDynamiteer/ 
+ * https://www.linkedin.com/in/johankampe/ 
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_11_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 32;
+            float tempFloat = 32.4f;
+
+            Console.WriteLine("{0} C = {1} F",
+                temp, MyTemperature.CelsiusToFahrenheit(temp));
+
+            Console.WriteLine("{0} C = {1} F",
+                tempFloat, MyTemperature.CelsiusToFahrenheit(tempFloat));
+
+            temp = 84;
+            tempFloat = 98.1f;
+
+            Console.WriteLine("{0} F = {1} C",
+                temp, MyTemperature.FahrenheitToCelcius(temp));
+
+            Console.WriteLine("{0} F = {1} C",
+                tempFloat, MyTemperature.FahrenheitToCelcius(tempFloat));
+
+            Console.ReadLine();
+        }
+    }
+}

ec347855c75ad0337622d54bed407a06c74d0c56
Johan Kämpe <kampe.johan@gmail.com>
2017-06-27 11:45:43 +0200

    Book exercises 12.3 - 12.4 done

---
+++ book_exercise_12_3_to_12_4/book_exercise_12_3_to_12_4/Program.cs
@@ -0,0 +1,80 @@
+﻿/* Programmering 1 C# Arbetsbok 
+ * Exercise 12.3 & 12.4 
+ * Page 25
+ *  
+ * Exercise notes: 
+ * Create a weather station with menu:
+ * [A]dd temperature
+ * [P]rint temperatures
+ * [R]emove temperature
+ * [Q]uit
+ * 
+ * When removing, enter list index
+ *   
+ * Johan Kämpe 
+ * https://github.com/GoblinDynamiteer/ 
+ * https://www.linkedin.com/in/johankampe/ 
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_12_3_to_12_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherStation Station = new WeatherStation();
+
+            char userInput = 's';
+
+            do
+            {
+                ShowMenu();
+                userInput = Console.ReadKey().KeyChar;
+
+                switch (char.ToLower(userInput))
+                {
+                    case 'a':
+                        Console.Write("\nEnter temperature: ");
+                        float temp;
+                        if (float.TryParse(Console.ReadLine(), out temp))
+                            Station.AddTemperature(temp);
+                        break;
+
+                    case 'r':
+                        Console.Write("\nEnter index to remove: ");
+                        int index;
+                        if (int.TryParse(Console.ReadLine(),out index))
+                            Station.removeTemperature(index);
+                        break;
+
+                    case 'p':
+                        Station.ListTemperatures();
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (Char.ToLower(userInput) != 'q');
+            
+        }
+
+        static void ShowMenu()
+        {
+            Console.Clear();
+            Console.Write("WeatherStation!\n" +
+                "\n[A]dd temperature" +
+                "\n[P]rint temperatures" +
+                "\n[R]emove temperature" +
+                "\n[Q]uit"+
+                "\n\nEnter Choice: ");
+        }
+    }
+}
---
+++ book_exercise_12_3_to_12_4/book_exercise_12_3_to_12_4/WeatherStation.cs
@@ -0,0 +1,69 @@
+﻿/* Programmering 1 C# Arbetsbok 
+ * Exercise 12.3 & 12.4 
+ * Page 25
+ *  
+ * Exercise notes: 
+ * Create a weather station with menu:
+ * [A]dd temperature
+ * [P]rint temperatures
+ * [R]emove temperature
+ * [Q]uit
+ * 
+ * When removing, enter list index
+ *   
+ * Johan Kämpe 
+ * https://github.com/GoblinDynamiteer/ 
+ * https://www.linkedin.com/in/johankampe/ 
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_12_3_to_12_4
+{
+    class WeatherStation
+    {
+        List<float> temperatures = new List<float>();
+
+        public void AddTemperature(float temp)
+        {
+            temperatures.Add(temp);
+        }
+
+        public void removeTemperature(int index)
+        {
+
+            if (index + 1 > temperatures.Count 
+                || index < 0)
+            {
+                Console.WriteLine("Index out of range!" + 
+                    "\nPress any key to continue!");
+                Console.ReadKey();
+                return;
+            }
+
+            /* Removes element at index */
+            temperatures.RemoveAt(index);
+            Console.WriteLine("Removed element at index {0}!", 
+                index);
+        }
+
+        public void ListTemperatures()
+        {
+            int index = 0;
+            Console.Clear();
+            Console.WriteLine("Recoreded temperaturs: ");
+            foreach(float temp in temperatures)
+            {
+                Console.WriteLine("[{0}]: {1}", index++, temp);
+            }
+
+            Console.WriteLine("\nPress any key to continue!");
+            Console.ReadKey();
+        }
+    }
+}

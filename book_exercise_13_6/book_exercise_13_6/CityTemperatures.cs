382242c4018045ff6d9af9c6f82fc1687dc89962
Johan Kämpe <kampe.johan@gmail.com>
2017-06-27 18:47:12 +0200

    Book exercice 13.6 done

---
+++ book_exercise_13_6/book_exercise_13_6/CityTemperatures.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exercise_13_6
+{
+    class City
+    {
+        private float temperature;
+
+        public float Temperature
+        {
+            get { return temperature; }
+            set { temperature = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public City(string name, float temperature)
+        {
+            this.temperature = temperature;
+            this.name = name;
+        }
+
+    }
+
+    class CityTemperatures
+    {
+        private List<City> cities = new List<City>();
+        private City minTempCity, maxTempCity;
+        private float averegeTemp = 0f;
+
+        public void AddCity(string name, float temperature)
+        {
+            cities.Add(new City(name, temperature));
+            setMinTemp();
+            setMaxTemp();
+            setAverageTemp();
+        }
+
+        public void ListCities()
+        {
+            if (!cities.Any()) {
+                Console.WriteLine("No cities in list!");
+                return;
+            }
+
+            Console.WriteLine(" Cities: \n");
+            foreach (City city in cities)
+            {
+                Console.WriteLine(" {0:N1}" +
+                    "\n  Temp: {1:N1}", 
+                        city.Name, city.Temperature);
+            }
+
+            Console.WriteLine("\n Average temp: {0}", averegeTemp);
+            Console.WriteLine(" Max temp: {0}, {1} degrees", 
+                maxTempCity.Name, 
+                maxTempCity.Temperature);
+            Console.WriteLine(" Min temp: {0}, {1} degrees",
+                minTempCity.Name,
+                minTempCity.Temperature);
+        }
+
+        private void setMinTemp()
+        {
+            minTempCity = cities[0];
+            foreach (City city in cities)
+            {
+                minTempCity = minTempCity.Temperature > city.Temperature ? 
+                    city : minTempCity;
+            }
+        }
+
+        private void setMaxTemp()
+        {
+            maxTempCity = cities[0];
+            foreach (City city in cities)
+            {
+                maxTempCity = maxTempCity.Temperature < city.Temperature ?
+                    city : maxTempCity;
+            }
+        }
+
+        private void setAverageTemp()
+        {
+            averegeTemp = 0f;
+
+            foreach (City city in cities)
+            {
+                averegeTemp += city.Temperature;
+            }
+
+            averegeTemp /= cities.Count();
+        }
+    }
+}
---
+++ book_exercise_13_6/book_exercise_13_6/Program.cs
@@ -0,0 +1,93 @@
+﻿/* Programmering 1 C# Arbetsbok 
+ * Exercise 13.6
+ * Page 28
+ *  
+ * Exercise notes: 
+ * Change exercise 7.3.4 to not have a set 
+ * amount of cities. 
+ * 
+ * The user shall be able to
+ *  - Add cities / temperature
+ *  - Show temeperature for all cities
+ *  - Show averege temperature
+ *  - Show coldest / warmest cities
+ *  
+ * The program shall handle incorrect inputs
+ *   
+ * Johan Kämpe 
+ * https://github.com/GoblinDynamiteer/ 
+ * https://www.linkedin.com/in/johankampe/ 
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace book_exercise_13_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CityTemperatures ct = new CityTemperatures();
+            bool quit = false;
+
+            do
+            {
+                Menu.ShowMenu();
+
+                switch (User.InputChar("\n Enter menu choice."))
+                {
+                    case '1':
+                        Console.Clear();
+                        string name = 
+                            User.InputString("Enter City name: ");
+                        float temp = 
+                            User.InputFloat("Enter temperature:");
+                        ct.AddCity(name, temp);
+                        break;
+
+                    case '2':
+                        Console.Clear();
+                        ct.ListCities();
+                        User.PressAnyKey("Press any key to continue.");
+                        break;
+
+                    case '3':
+                        quit = true;
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+            } while (!quit);
+
+        }
+    }
+
+    class Menu
+    {
+        static string [] menuItems = {
+            "Add city", "Show temperatures", "Quit" };
+
+        public static void ShowMenu()
+        {
+            int itemNumber = 1;
+
+            Console.Clear();
+
+            foreach (string item in menuItems)
+            {
+                Console.Write("[{0}] ", itemNumber++);
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+

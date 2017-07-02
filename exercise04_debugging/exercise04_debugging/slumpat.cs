7d52aedda22c7f34228c182828da4b9d8d8d0567
Johan Kämpe <kampe.johan@gmail.com>
2017-07-02 10:33:13 +0200

    Added highscore system

--- exercise04_debugging/exercise04_debugging/slumpat.cs
+++ exercise04_debugging/exercise04_debugging/slumpat.cs
@@ -9,11 +9,24 @@
  *      why the original code was wrong
  *  
  * Extra:
- *  -   Allow the user replay the game
+ *  -   Allow the user to replay the game
  *  -   Count guesses
  *  -   High Score list, that saves number of guesses
  *      between sessions
  *
+ * 
+ * Notes:
+ *  - I'm using my highscore class library that was made
+ *    for the Programmering 1 C# Arbetsbok exercise 6 on
+ *    page 37.
+ *    
+ *    I copied the class library code into this file, as
+ *    the hand in for this exercise doesn't allow multiple
+ *    files.
+ *    
+ *    Link to highscore class library exercise solution:
+ *    https://github.com/GoblinDynamiteer/P1CS/tree/master/book_complex_exercise_6
+ * 
  * Johan Kämpe
  * 2017-07-01
  * https://github.com/GoblinDynamiteer/  
@@ -28,6 +41,11 @@
  * Solution:
  * Changed system -> System,  uppercase S   */
 using System;
+using Game; // Highscore class library
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 namespace Uppgift_4
 {
@@ -36,6 +54,7 @@ namespace Uppgift_4
         static void Main(string[] args)
         {
             Random slumpat = new Random();
+            HighScore highscore = new HighScore();
 
             /* Error:
             * Random.Next() without passed parameters
@@ -44,7 +63,7 @@ namespace Uppgift_4
             * Solution:
             * Pass parameters to randomize between 1 and 20  */
             int speltal = slumpat.Next(1, 21);
-
+            int guessCount = 0; // Count guesses
             bool spela = true;
 
             /* Error:
@@ -75,15 +94,16 @@ namespace Uppgift_4
                 */
 
                 int tal;
+                guessCount++;
 
                 do
                 {
                     Console.Write(
                         "\n\tGissa på ett tal mellan 1 och 20: ");
                 } while (!int.TryParse( //while conversion is NOT successful
-                    Console.ReadLine(), out tal)); 
-                
-                
+                    Console.ReadLine(), out tal));
+
+
                 if (tal < speltal)
                 {
                     Console.WriteLine("\tDet inmatade talet " 
@@ -110,7 +130,14 @@ namespace Uppgift_4
                  */
                 if (tal == speltal)
                 {
-                    Console.WriteLine("\tGrattis, du gissade rätt!");
+                    Console.WriteLine(
+                        "\tGrattis, du gissade rätt! ({0} antal försök)", 
+                            guessCount);
+
+                    Console.Write("Ange ditt namn för highscore: ");
+                    highscore.Add(Console.ReadLine(), guessCount);
+
+                    highscore.Print();
 
                     /* Error:
                     * Program quit after first guess
@@ -120,8 +147,12 @@ namespace Uppgift_4
                     * was include "spela = false;
                     */
                     spela = false;
+                    guessCount = 0;
                 }
             }
+
+            /* Save highscores to file */
+            highscore.Save();
 
             /* Added ReadLine to prevent console window from 
              * closing after winning the game */
@@ -129,3 +160,168 @@ namespace Uppgift_4
         }
     }
 }
+
+namespace Game
+{
+    [Serializable]
+    /* High Score item class */
+    class HSItem : IComparable
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int points;
+        public int Points
+        {
+            get { return points; }
+            set { points = value; }
+        }
+
+        /* Constructor */
+        public HSItem(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        /* For list sorting */
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            HSItem otherItem = obj as HSItem;
+            if (otherItem != null)
+                return this.points.CompareTo(otherItem.points);
+            else
+                throw new ArgumentException("Object is not Points!");
+        }
+
+    }
+
+    class HighScore
+    {
+        int maxItems;
+        bool descendingSort; // Ascending or descending sort
+        string filename = "highscore.dat";
+        List<HSItem> highScoreList;
+
+        /* Constructor */
+        public HighScore(int maxItems = 10, bool descendingSort = true)
+        {
+            this.maxItems = maxItems;
+            this.descendingSort = descendingSort;
+            this.highScoreList = new List<HSItem>();
+        }
+
+        /* Add name and score to list */
+        public void Add(string name, int points = 0)
+        {
+            if (name == "")
+            {
+                name = "NO NAME";
+            }
+
+            highScoreList.Add(new HSItem(name, points));
+            Sort();
+        }
+
+        /* Print list, for testing */
+        public void Print()
+        {
+            Console.WriteLine(
+                "******** HIGHSCORE ********");
+            int place = 1;
+
+            foreach (HSItem score in highScoreList)
+            {
+                Console.WriteLine(
+                    "{0}. {1} {2} Gissningar",
+                        place++, score.Name, score.Points);
+            }
+        }
+
+        /* Sort list and remove item if needed */
+        private void Sort()
+        {
+            highScoreList.Sort();
+
+            /* Ascending / descending */
+            if (descendingSort)
+            {
+                /* Reverse sorted list, if largest value
+                 * shall be on top */
+                highScoreList.Reverse();
+            }
+
+            /* Remove if over max items */
+            while (highScoreList.Count > maxItems)
+            {
+                highScoreList.RemoveAt(highScoreList.Count - 1);
+            }
+        }
+
+        /* Save high scores to file */
+        public void Save()
+        {
+            FileStream datafile = new FileStream(
+                this.filename, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            try
+            {
+                formatter.Serialize(datafile, highScoreList);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to save: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                datafile.Close();
+            }
+        }
+
+        /* Load high scores from file */
+        public void Load()
+        {
+
+            FileStream datafile;
+
+            try
+            {
+                datafile = new FileStream(
+                    this.filename, FileMode.Open);
+            }
+
+            /* If datafile does not exist. */
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("No file: " + e.Message);
+                return;
+            }
+
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                /* Overwrites list if it contains contents */
+                highScoreList = (List<HSItem>)formatter.Deserialize(datafile);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to load: " + e.Message);
+                return;
+            }
+
+            datafile.Close();
+
+        }
+    }
+}
+

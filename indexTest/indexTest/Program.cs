43f21e9d2920e1a187e82cbf6d03ff228d4b11fa
Johan Kämpe <kampe.johan@gmail.com>
2017-07-05 12:28:33 +0200

    Array index out of range test

---
+++ indexTest/indexTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heltal = new int[4] { 1, 2, 3, 4 };
+
+            for (int i = 0; i < heltal.Length + 10 ; i++)
+            {
+                try
+                {
+                    Console.WriteLine(heltal[i]);
+                }
+
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("index " + i + " är utanför array!");
+                }
+
+            }
+
+            heltal[7] = 2;
+
+            Console.ReadLine();
+        }
+
+    }
+}

71fc525147569cec2b92358c4cba9dfafc3b17b3
Johan Kämpe <kampe.johan@gmail.com>
2017-07-12 11:17:35 +0200

    Exercise 07 - The logbook, init

---
+++ exercise07_logbook/exercise07_logbook/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Examinerande uppgift - Loggboken
+ * Programmering 1
+ * 
+ * Instructions:
+ *  - Create a logbook based on pseudocode
+ *    from exercise 3.
+ *  - Study the pseudo code, what changes are
+ *    needed? Update and write documentation.
+ *  
+ *  Needed:
+ *   - A main meny, using switch-case
+ *   - Prevent runtime errors
+ *   - Print logbook contents
+ *   - Save new log entries (Use lists/arrays)
+ *   - Search log entries (Implement search algorithm)
+ *   
+ *  Extras:
+ *   - Time/Date for log entries
+ *   - Edit/delete log entries
+ *   - Create one or several methods
+ *  
+ * Johan Kämpe
+ * 2017-07-12
+ * https://github.com/GoblinDynamiteer/  
+ * https://www.linkedin.com/in/johankampe/  
+ *   
+ */
+
+namespace exercise07_logbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /* Log book entries as class */
+    class Entry
+    {
+
+    }
+}

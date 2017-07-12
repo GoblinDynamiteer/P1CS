526037c1fec10862c60efce4ce56253437997479
Johan Kämpe <kampe.johan@gmail.com>
2017-07-12 11:18:29 +0200

    List test

--- indexTest/indexTest/Program.cs
+++ indexTest/indexTest/Program.cs
@@ -11,6 +11,7 @@ namespace indexTest
         static void Main(string[] args)
         {
             int[] heltal = new int[4] { 1, 2, 3, 4 };
+            List<int> heltalLista = new List<int>();
 
             for (int i = 0; i < heltal.Length + 10 ; i++)
             {
@@ -26,7 +27,27 @@ namespace indexTest
 
             }
 
-            heltal[7] = 2;
+            for (int i = 0; i < 30; i++)
+            {
+                heltalLista.Add(i+30);
+            }
+
+            for (int i = 0; i < heltalLista.Count; i++)
+            {
+                Console.WriteLine(heltalLista[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(heltalLista.ElementAt(4));
+
+            Console.WriteLine();
+
+            List<int> nyLista = heltalLista.GetRange(10, 10);
+            for (int i = 0; i < nyLista.Count; i++)
+            {
+                Console.WriteLine(nyLista[i]);
+            }
 
             Console.ReadLine();
         }
